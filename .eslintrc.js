76818873a7e80d06c151b7b10a9e52999b629b0e
Carter J. Bastian <carter.bastian1@gmail.com>
2020-04-23 13:24:21 -0700

    Add basic linters and such

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+// .eslintrc.js
+module.exports = {
+  env: {
+    node: true,
+    mocha: true,
+    es6: true,
+    commonjs: true,
+  },
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2018,
+  },
+  rules: {
+    curly: [2, 'all'],
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    'comma-dangle': ['error', 'always-multiline'],
+    'max-len': [2, { code: 100, tabWidth: 2, ignoreUrls: true }],
+    'prettier/prettier': 'off',
+  },
+}
