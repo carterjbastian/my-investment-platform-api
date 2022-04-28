8d91f9cea174e72ac298b03c953f19dd391a8cf6
Carter Bastian <carter.bastian1@gmail.com>
2022-04-28 14:30:36 -0700

    Get mongo config fixed

--- src/initMongo.js
+++ src/initMongo.js
@@ -4,10 +4,6 @@ const DB_NAME = 'baseServer'
 const DB_URI = process.env.MONGODB_URI || `mongodb://localhost:27017/${DB_NAME}`
 
 module.exports = () => {
-  mongoose.connect(DB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-  })
+  mongoose.connect(DB_URI, {})
   mongoose.Promise = require('bluebird')
 }
